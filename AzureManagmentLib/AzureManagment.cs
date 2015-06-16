5d8d83d31329018bc88d69457a3007edc12cb143
Paulius Norkus <paulius.nrk@gmail.com>
2015-06-16 11:29:47 +0300

    init

---
+++ AzureManagmentLib/AzureManagment.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Management.Compute;
+using Microsoft.WindowsAzure.Management.Compute.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace AzureManagmentLib
+{
+    public class AzureManagment
+    {
+        public const string CERT = "<ManagementCertificate Base64>";
+        public const string SUBSCRIPTION_ID = "<Subscription Id>";
+
+        private static CertificateCloudCredentials credentials;
+        public static CertificateCloudCredentials Credentials
+        {
+            get
+            {
+                if (credentials == null)
+                {
+                    var cert = new X509Certificate2(Convert.FromBase64String(CERT));
+                    credentials = new CertificateCloudCredentials(SUBSCRIPTION_ID, cert);
+                }
+                return credentials;
+            }
+        }
+
+
+        public async Task<List<CloudService>> GetCloudDeployments()
+        {
+            List<CloudService> result = new List<CloudService>();
+            using (var computeClient = new ComputeManagementClient(Credentials))
+            {
+                var cs = await computeClient.HostedServices.ListAsync();
+                foreach (var c in cs)
+                {
+                    var service = new CloudService { HostedService = c, Deployments = new Dictionary<DeploymentSlot, DeploymentGetResponse>() };
+                    foreach (var slot in Enum.GetValues(typeof(DeploymentSlot)).Cast<DeploymentSlot>())
+                    {
+                        var deployment = await GetDeploymnet(computeClient, c.ServiceName, slot);
+                        service.Deployments.Add(slot, deployment);
+                    }
+                    result.Add(service);
+                }
+            }
+            return result;
+        }
+
+        public static async Task<DeploymentGetResponse> GetDeploymnet(ComputeManagementClient computeClient, string serviceName, DeploymentSlot slot)
+        {
+            DeploymentGetResponse d = null;
+            try
+            {
+                d = await computeClient.Deployments.GetBySlotAsync(serviceName, slot);
+            }
+            catch (Exception e) { }
+            return d;
+        }
+    }
+
+    public class CloudService
+    {
+        public HostedServiceListResponse.HostedService HostedService { get; set; }
+        public Dictionary<DeploymentSlot, DeploymentGetResponse> Deployments { get; set; }
+    }
+}
---
+++ AzureManagmentLib/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AzureManagmentLib
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var managment = new AzureManagment();
+            var cloudServices = managment.GetCloudDeployments().Result;
+
+            foreach (var cs in cloudServices)
+            {
+                foreach (var d in cs.Deployments)
+                {
+                    Console.WriteLine(string.Format("CloudService={0}; Slot={1}; DeploymentName={2}", 
+                        cs.HostedService.ServiceName, d.Key, d.Value != null ? d.Value.Name : "NULL"));
+                }
+            }
+        }
+    }
+}
